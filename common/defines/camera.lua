816dad2b38fff164e87bd3ddb4cb8e3bc597fa30
MemeWizard <46576860+MemeWizard@users.noreply.github.com>
2019-12-23 11:17:40 +0300

    Create camera.lua
    gdc

---
+++ common/defines/camera.lua
@@ -0,0 +1,44 @@
+-- Initial position in main menu
+NDefines.NFrontend.CAMERA_START_X = 790.0
+NDefines.NFrontend.CAMERA_START_Y = 150.0
+NDefines.NFrontend.CAMERA_START_Z = 500.0
+-- Move to position in main menu
+NDefines.NFrontend.CAMERA_END_X = 790.0
+NDefines.NFrontend.CAMERA_END_Y = 500.0
+NDefines.NFrontend.CAMERA_END_Z = 600.0
+-- Rotation point in main menu
+NDefines.NFrontend.CAMERA_LOOKAT_X = 790.0
+NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1000.0
+-- ???
+NDefines.NFrontend.FRONTEND_POS_X = 790.0
+NDefines.NFrontend.FRONTEND_POS_Y = 500.0
+NDefines.NFrontend.FRONTEND_POS_Z = 600.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 790.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1000.0
+-- Move to position in settings
+NDefines.NFrontend.SETTINGS_POS_X = 790.0
+NDefines.NFrontend.SETTINGS_POS_Y = 500.0
+NDefines.NFrontend.SETTINGS_POS_Z = 400.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 1400.0
+NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 648.0
+-- Rotation point in settings
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1400.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 648.0
+-- Move to position in multiplayer
+NDefines.NFrontend.MP_OPTIONS_POS_X = 360.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 200.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 848.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 490.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1200.0
+-- Move to position in credits
+NDefines.NFrontend.CREDITS_POS_X = 1000.0
+NDefines.NFrontend.CREDITS_POS_Y = 200.0
+NDefines.NFrontend.CREDITS_POS_Z = 1308.0
+NDefines.NFrontend.CREDITS_LOOK_X = 900.0
+NDefines.NFrontend.CREDITS_LOOK_Y = 0.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 1450.0
