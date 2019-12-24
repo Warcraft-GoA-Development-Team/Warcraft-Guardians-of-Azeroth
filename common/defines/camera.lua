0119615458be2e67d12b4bcb6a08f1838ca638d9
MemeWizard <46576860+MemeWizard@users.noreply.github.com>
2019-12-24 14:28:20 +0300

    settings stump

--- common/defines/camera.lua
+++ common/defines/camera.lua
@@ -7,34 +7,35 @@
 NDefines.NFrontend.FRONTEND_POS_X = 825.0
 NDefines.NFrontend.FRONTEND_POS_Y = 500.0
 NDefines.NFrontend.FRONTEND_POS_Z = 1075.0
--- Camera angle
 NDefines.NFrontend.FRONTEND_LOOK_X = 800.0
 NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
 NDefines.NFrontend.FRONTEND_LOOK_Z = 1275.0
 
 -- Initial position in main menu
-NDefines.NFrontend.CAMERA_START_X = 790.0
-NDefines.NFrontend.CAMERA_START_Y = 150.0
-NDefines.NFrontend.CAMERA_START_Z = 500.0
+NDefines.NFrontend.CAMERA_START_X = 1300.0
+NDefines.NFrontend.CAMERA_START_Y = 100.0
+NDefines.NFrontend.CAMERA_START_Z = 1500.0
 -- Move to position in main menu
-NDefines.NFrontend.CAMERA_END_X = 790.0
+NDefines.NFrontend.CAMERA_END_X = 825.0
 NDefines.NFrontend.CAMERA_END_Y = 500.0
-NDefines.NFrontend.CAMERA_END_Z = 600.0
+NDefines.NFrontend.CAMERA_END_Z = 1500.0
 -- Rotation point in main menu
-NDefines.NFrontend.CAMERA_LOOKAT_X = 790.0
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1300.0
 NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
-NDefines.NFrontend.CAMERA_LOOKAT_Z = 1000.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1500.0
+
 -- Move to position in settings
-NDefines.NFrontend.SETTINGS_POS_X = 790.0
-NDefines.NFrontend.SETTINGS_POS_Y = 500.0
-NDefines.NFrontend.SETTINGS_POS_Z = 400.0
-NDefines.NFrontend.SETTINGS_LOOK_X = 1400.0
+NDefines.NFrontend.SETTINGS_POS_X = 1815.0
+NDefines.NFrontend.SETTINGS_POS_Y = 50.0
+NDefines.NFrontend.SETTINGS_POS_Z = 1930.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 1875.0
 NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
-NDefines.NFrontend.SETTINGS_LOOK_Z = 648.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 2010.0
 -- Rotation point in settings
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1400.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1875.0
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 648.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 2010.0
+
 -- Move to position in multiplayer
 NDefines.NFrontend.MP_OPTIONS_POS_X = 360.0
 NDefines.NFrontend.MP_OPTIONS_POS_Y = 200.0
@@ -42,6 +43,7 @@ NDefines.NFrontend.MP_OPTIONS_POS_Z = 848.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_X = 490.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1200.0
+
 -- Move to position in credits
 NDefines.NFrontend.CREDITS_POS_X = 1000.0
 NDefines.NFrontend.CREDITS_POS_Y = 200.0
