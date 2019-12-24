70e28a51733dc6c0774d0cc237f8be2c08836883
MemeWizard <46576860+MemeWizard@users.noreply.github.com>
2019-12-24 15:51:48 +0300

    switched creadits and settings
    gdc

--- common/defines/camera.lua
+++ common/defines/camera.lua
@@ -25,16 +25,16 @@ NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
 NDefines.NFrontend.CAMERA_LOOKAT_Z = 1500.0
 
 -- Move to position in settings
-NDefines.NFrontend.SETTINGS_POS_X = 1815.0
-NDefines.NFrontend.SETTINGS_POS_Y = 50.0
-NDefines.NFrontend.SETTINGS_POS_Z = 1930.0
-NDefines.NFrontend.SETTINGS_LOOK_X = 1875.0
+NDefines.NFrontend.SETTINGS_POS_X = 580.0
+NDefines.NFrontend.SETTINGS_POS_Y = 125.0
+NDefines.NFrontend.SETTINGS_POS_Z = 650.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 560.0
 NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
-NDefines.NFrontend.SETTINGS_LOOK_Z = 2010.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 800.0
 -- Rotation point in settings
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1875.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 560.0
 NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 2010.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 800.0
 
 -- Move to position in multiplayer
 NDefines.NFrontend.MP_OPTIONS_POS_X = 620.0
@@ -45,9 +45,9 @@ NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1650.0
 
 -- Move to position in credits
-NDefines.NFrontend.CREDITS_POS_X = 580.0
-NDefines.NFrontend.CREDITS_POS_Y = 125.0
-NDefines.NFrontend.CREDITS_POS_Z = 650.0
-NDefines.NFrontend.CREDITS_LOOK_X = 560.0
+NDefines.NFrontend.CREDITS_POS_X = 1815.0
+NDefines.NFrontend.CREDITS_POS_Y = 50.0
+NDefines.NFrontend.CREDITS_POS_Z = 1930.0
+NDefines.NFrontend.CREDITS_LOOK_X = 1875.0
 NDefines.NFrontend.CREDITS_LOOK_Y = 0.0
-NDefines.NFrontend.CREDITS_LOOK_Z = 800.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 2010.0
